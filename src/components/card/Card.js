44339c6d2018ebcf0181ef624b41105af77bee62
Hossein <hosseinfti@yahoo.com>
2022-06-01 18:34:59 +0430

    border for card-body

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,10 +1,15 @@
 import React, { Component } from "react";
 import "./card.scss";
 
-const instagram = require("../../assets/Instagram.svg").default;
+const instagram = require("../../assets/black-instagram.svg").default;
 const like = require("../../assets/empty-heart.svg").default;
 const comment = require("../../assets/empty-comment.svg").default;
-const date = Date();
+
+const time = new Date();
+const date = new Intl.DateTimeFormat("fa-IR", {
+  hour: "2-digit",
+  minute: "2-digit",
+}).format(time);
 
 class Card extends Component {
   render() {
@@ -14,18 +19,19 @@ class Card extends Component {
           <div className="card-header">
             <div className="card-header-icon">
               {" "}
-              <img src={instagram} width="20px" />{" "}
+              <img src={instagram} width="18px" />{" "}
             </div>
           </div>
-          <div className="card-body"></div>
+          <div className="card-body">
+            بمب فردا یکشنبه 😍😍 تیشرت تک پسرانه با کیفیت عالی سایز :
+            45-50-55-60 مناسب 4 سال تا 12 سال با قیمت استثنایی فقط 89 هزار تومان
+          </div>
           <div className="card-footer">
             <div>
-              <span className="card-footer-like">
-                <img src={like} width="20px" />
-              </span>
+                <img className="card-footer-like" src={like} width="18px" />
               <span className="card-footer-comment">
                 {" "}
-                <img src={comment} width="20px" />{" "}
+                <img src={comment} width="18px" />{" "}
               </span>
             </div>
             <div className="card-footer-date">{date}</div>
